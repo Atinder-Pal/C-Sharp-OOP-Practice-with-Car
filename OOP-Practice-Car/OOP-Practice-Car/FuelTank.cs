2b4262b3997e6b4312845c7b1837261575a4d4b8
Atinder-Pal <atty.pal46@gmail.com>
2020-10-22 16:49:04 -0600

    Create FuelTank Class; Test its methods;

---
+++ OOP-Practice-Car/OOP-Practice-Car/FuelTank.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Practice_Car
+{
+    class FuelTank
+    {
+        private float _capacity;
+        private float _level;
+
+        //Property Capacity
+        public float Capacity 
+        {
+            get
+            {
+                return _capacity;
+            }
+            private set
+            {
+                if( value < 0 )
+                {
+                    throw new Exception("Capacity cannot be less than 0");
+                }
+                _capacity = value;
+            }
+        }
+
+        //Property Level
+        public float Level 
+        {
+            get
+            {
+                return _level;
+            }
+            private set
+            {
+                if( value < 0 )
+                {
+                    throw new Exception("Fuel Level cannot go in Negative");
+                }
+                else if( value > Capacity )
+                {
+                    _level = Capacity;
+                }
+                else
+                {
+                    _level = value;
+                }
+            }
+        }
+
+        //Methods
+        public void BurnFuel( float fuelVolume )
+        {
+            Level -= fuelVolume;
+        }
+        public void Fill( float fuelVolume )
+        {
+            Level += fuelVolume;
+        }
+
+        //Deafult Constructor
+        public FuelTank()
+        {
+            Capacity = 50;
+            Level = 50;
+        }
+    }
+}
--- OOP-Practice-Car/OOP-Practice-Car/Program.cs
+++ OOP-Practice-Car/OOP-Practice-Car/Program.cs
@@ -6,12 +6,25 @@ namespace OOP_Practice_Car
     {
         static void Main(string[] args)
         {
-            //Testing the Odometer class and it's methods
-            Odometer odo = new Odometer();
-            odo.Increment(999980);
-            Console.WriteLine( odo.Counter );
-            odo.Increment(40);
-            Console.WriteLine(odo.Counter);
+            //Testing the Odometer class and its methods
+            //Odometer odo = new Odometer();
+            //odo.Increment(999980);
+            //Console.WriteLine( odo.Counter );
+            //odo.Increment(40);
+            //Console.WriteLine(odo.Counter);
+
+            //Testing FuelTank Class and its methods
+            FuelTank testFueltank = new FuelTank();
+
+            // Citation
+            //https://stackoverflow.com/questions/19671667/cannot-implicitly-convert-type-double-to-float
+            //Added 'f' to explicitly specify th ecompiler that this number is a float. It was giving error- cannot implicitly convert double to float as it assumes that  numbers which include a decimal part are double.
+            testFueltank.BurnFuel(9.8f); 
+            //End Citation
+            Console.WriteLine( testFueltank.Capacity );
+            Console.WriteLine(testFueltank.Level);
+            testFueltank.BurnFuel(40.2f);
+            Console.WriteLine(testFueltank.Level);
         }
     }
 }
