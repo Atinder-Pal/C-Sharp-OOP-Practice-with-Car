f8f6224c170c092bbae12085fa465efb8cd41cd3
Atinder-Pal <atty.pal46@gmail.com>
2020-10-22 16:18:04 -0600

    Create Class Odometer;Test its methods;

---
+++ OOP-Practice-Car/OOP-Practice-Car/Odometer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Practice_Car
+{
+    class Odometer
+    {
+        private const int threshold = 999999;
+        private int _counter;
+
+        //Property "Counter"
+        public int Counter  
+        {
+            get
+            {
+                return _counter;
+            }
+            private set
+            {
+                if( value > threshold )
+                {
+                    _counter = value - threshold -1;
+                }
+                else if(value < 0 )
+                {
+                    throw new Exception("Car cannot have negative Odometer");
+                }
+                else
+                {
+                    _counter = value;
+                }
+            }
+        }
+
+        //Methods
+        public void Increment(int distance)
+        {
+            Counter += distance;
+        }
+
+        //Default Constructor
+        public Odometer()
+        {
+            Counter = 0;
+        }
+    }
+}
---
+++ OOP-Practice-Car/OOP-Practice-Car/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace OOP_Practice_Car
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Testing the Odometer class and it's methods
+            Odometer odo = new Odometer();
+            odo.Increment(999980);
+            Console.WriteLine( odo.Counter );
+            odo.Increment(40);
+            Console.WriteLine(odo.Counter);
+        }
+    }
+}
